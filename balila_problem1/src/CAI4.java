a190251d0a717567969da40fb70f542b6af3e6f5
Joshua Balila <joshuabalila55@gmail.com>
2020-02-10 17:11:36 -0500

    Copied skeleton code from CAI3 to CAI4

--- balila_problem1/src/CAI4.java
+++ balila_problem1/src/CAI4.java
@@ -1,5 +1,176 @@
+/*
+ * Change CAI3 to allow for different difficulty settings
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * quiz() method contains program logic
+ * 
+ * askQuestion() method prints problem
+ * 		Uses rand to generate two numbers
+ * readResponse() method reads answer
+ * isAnswerCorrect() method verifies answer
+ * 		displayCorrectResponse() method displays "Very good!"
+ * 			Randomly select 1 of 4 responses for correct answer
+ * 		displayIncorrectResponse() method displays "No. Please try again."
+ * 			Randomly select 1 of 4 responses for incorrect answer
+ * 
+ * Create main method to test program
+ */
+import java.security.SecureRandom;
+import java.util.Scanner;
 
 public class CAI4 
 {
+// Define constant to limit number of questions asked
+private final int NUM_QUESTIONS = 10;
 
+// Instance variables to hold values for multiplication
+private int num1;
+private int num2;
+private int answer;
+private int response;
+private int correct;
+private int incorrect;
+private double percentage;
+private SecureRandom rand = new SecureRandom();
+private Scanner stdin = new Scanner(System.in);
+
+// Assigns num1 and num2 to values and outputs problem to screen
+private void askQuestion()
+{
+	// Random numbers in the range of 0-9 inclusive
+	num1 = rand.nextInt(10);
+	num2 = rand.nextInt(10);
+	
+	// Calculates answer and stores in instance variable
+	answer = num1 * num2;
+	
+	// Outputs question to screen
+	System.out.println("How much is " + num1 + " times " + num2 + "?");
+}
+
+// Reads input from User
+private void readResponse()
+{
+	response = stdin.nextInt();
+}
+
+// Checks if User input equals the correct answer
+private boolean isAnswerCorrect()
+{
+	if (answer == response)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// Randomly selects 1 of 4 responses upon receiving a correct answer
+private void displayCorrectResponse()
+{
+	int choice = rand.nextInt(4);
+	switch(choice)
+	{
+	case 0:
+		System.out.println("Very good!");
+		break;
+	case 1:
+		System.out.println("Excellent!");
+		break;
+	case 2:
+		System.out.println("Nice work!");
+		break;
+	case 3:
+		System.out.println("Keep up the good work!");
+		break;
+	default:
+		// Should not reach default case based on range of choice (0 - 3)
+		break;
+	}
+}
+
+// Randomly selects 1 of 4 responses upon receiving an incorrect answer
+private void displayIncorrectResponse()
+{
+	int choice = rand.nextInt(4);
+	switch(choice)
+	{
+	case 0:
+		System.out.println("No. Please try again.");
+		break;
+	case 1:
+		System.out.println("Wrong. Try once more.");
+		break;
+	case 2:
+		System.out.println("Don't give up!");
+		break;
+	case 3:
+		System.out.println("No. Keep trying.");
+		break;
+	default:
+		// Should not reach default case based on range of choice (0 - 3)
+		break;
+	}
+}
+
+// Displays percentage of student based on correct and incorrect answers
+private void displayCompletionMessage()
+{
+	percentage = (double)correct/NUM_QUESTIONS;
+	System.out.printf("Your score was %.0f%%.\n", percentage * 100);
+	
+	if (percentage < 0.75)
+	{
+		System.out.println("Please ask your teacher for extra help.");
+	}
+	else
+	{
+		System.out.println("Congratulations, you are ready to go to the next level!");
+	}
+}
+
+// Prints multiplication question to screen and loops until correct response given
+public void quiz()
+{	
+	char choice;
+	
+	do
+	{
+		// Resets correct/incorrect responses to 0
+		correct = 0;
+		incorrect = 0;
+		
+		for (int i = 0; i < NUM_QUESTIONS; i++)
+		{
+			askQuestion();
+			readResponse();
+			if (isAnswerCorrect() == true)
+			{
+				correct++;
+				displayCorrectResponse();
+			}
+			else
+			{
+				incorrect++;
+				displayIncorrectResponse();
+			}
+		}
+		
+		displayCompletionMessage();
+		
+		System.out.println("Would you like to solve another problem? (Y/N)");
+		choice = stdin.next().charAt(0);
+	} while(choice == 'y' || choice == 'Y');
+}
+
+// To test CAI1 class
+public static void main(String[] args)
+{
+	CAI3 student = new CAI3();
+	
+	student.quiz();
+}
 }
