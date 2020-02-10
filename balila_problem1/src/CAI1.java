3f6f5a9694009501b81d02f558f5fe22afabd5f4
Joshua Balila <joshuabalila55@gmail.com>
2020-02-10 14:23:25 -0500

    Initial Commit

---
+++ balila_problem1/src/CAI1.java
@@ -0,0 +1,29 @@
+/*
+ * Generate rand numbers with SecureRandom object
+ * 
+ * Ask student to solve multiplication question
+ * 		Two numbers in the range from 0 - 9 (inclusive)
+ * 
+ * Display "Very good!" if correct
+ * Display "No. Please try again." if incorrect
+ * 
+ * Program ends when correct response is given
+ * 		Program asks problem repeatedly until correct response given
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * quiz() method contains program logic
+ * 
+ * askQuestion() method prints problem
+ * 		Uses rand to generate two numbers
+ * readResponse() method reads answer
+ * isAnswerCorrect() method verifies answer
+ * 		displayCorrectResponse() method displays "Very good!"
+ * 		displayIncorrectResponse() method displays "No. Please try again."
+ * 
+ * Create main method to test program
+ */
+public class CAI1 
+{
+	
+}
---
+++ balila_problem2/src/SavingsAccountTest.java
@@ -0,0 +1,11 @@
+
+public class SavingsAccountTest 
+{
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
