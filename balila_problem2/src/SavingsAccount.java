3a4c0cf3480dee2ae639bc6473fd34d476bdd36f
Joshua Balila <joshuabalila55@gmail.com>
2020-02-10 20:57:55 -0500

    Create SavingsAccount class

--- balila_problem2/src/SavingsAccount.java
+++ balila_problem2/src/SavingsAccount.java
@@ -1,5 +1,21 @@
 
 public class SavingsAccount 
 {
-
+	private static double annualInterestRate;
+	private double savingsBalance;
+	
+	public SavingsAccount(double s)
+	{
+		savingsBalance = s;
+	}
+	
+	public static void modifyInterestRate(double rate)
+	{
+		annualInterestRate = rate;
+	}
+	
+	public void calculateMonthlyInterest()
+	{
+		savingsBalance = (savingsBalance * annualInterestRate) / 12;
+	}
 }
