b9f1d595ab4d08b8ab15ed90b101f5af8c814e33
Joshua Balila <joshuabalila55@gmail.com>
2020-02-10 21:10:02 -0500

    Created Tester class for SavingsAccount

--- balila_problem2/src/SavingsAccount.java
+++ balila_problem2/src/SavingsAccount.java
@@ -1,21 +1,42 @@
-
 public class SavingsAccount 
 {
+	// Instance variables for SavingsAccount
 	private static double annualInterestRate;
 	private double savingsBalance;
 	
+	// Constructor to instantiate savingsBalance variable
 	public SavingsAccount(double s)
 	{
-		savingsBalance = s;
+		setSavingsBalance(s);
 	}
 	
+	// Sets annualInterestRate
 	public static void modifyInterestRate(double rate)
 	{
 		annualInterestRate = rate;
 	}
 	
+	// Calculates one month's interest based on the annual interest rate
 	public void calculateMonthlyInterest()
 	{
-		savingsBalance = (savingsBalance * annualInterestRate) / 12;
+		savingsBalance += (savingsBalance * annualInterestRate) / 12.0;
+	}
+	
+	// Sets savingsBalance and checks to make sure that it is greater than or equal to 0
+	public void setSavingsBalance(double balance)
+	{
+		if (balance < 0)
+		{
+			System.out.println("Balance cannot be negative.");
+			return;
+		}
+		
+		savingsBalance = balance;
+	}
+	
+	// Returns savingsBalance
+	public double getSavingsBalance()
+	{
+		return savingsBalance;
 	}
 }
--- balila_problem2/src/SavingsAccountTest.java
+++ balila_problem2/src/SavingsAccountTest.java
@@ -4,8 +4,30 @@ public class SavingsAccountTest
 
 	public static void main(String[] args) 
 	{
+		// Instantiates two SavingsAccount objects
+		SavingsAccount saver1 = new SavingsAccount(2000);
+		SavingsAccount saver2 = new SavingsAccount(3000);
 		
-
+		// Sets annualInterestRate to 0.04 (4%)
+		SavingsAccount.modifyInterestRate(0.04);
+		
+		// Calculates monthly interest 12 times over course of 1 year
+		for (int i = 0; i < 12; i++)
+		{
+			saver1.calculateMonthlyInterest();
+			saver2.calculateMonthlyInterest();
+		}
+		
+		// Sets annualInterestRate to 0.05 (5%)
+		SavingsAccount.modifyInterestRate(0.05);
+		
+		// Calculates next month's interest
+		saver1.calculateMonthlyInterest();
+		saver2.calculateMonthlyInterest();
+		
+		// Prints out current balances for saver1 and saver2
+		System.out.printf("Saver1's balance is $%.2f\n", saver1.getSavingsBalance());
+		System.out.printf("Saver2's balance is $%.2f\n", saver2.getSavingsBalance());
 	}
 
 }
